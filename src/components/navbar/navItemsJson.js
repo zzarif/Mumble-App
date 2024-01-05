fc92e8332e680b8950ac34158c35ce8b74c80b30
Zibran Zarif <zzamio1112@gmail.com>
2024-01-05 23:10:51 +0600

    login api fix

---
+++ src/components/navbar/navItemsJson.js
@@ -0,0 +1,44 @@
+export const navItems = [
+    {
+        "id": 0,
+        "title": "Registration",
+        "path": "#/registration",
+        "imageLink": "/navbar/woman.png"
+    },
+    {
+        "id": 1,
+        "title": "Reports",
+        "path": "#/reports",
+        "imageLink": "/navbar/clipboard.png"
+    },
+    {
+        "id": 2,
+        "title": "Dispatches",
+        "path": "#/dispatches",
+        "imageLink": "/navbar/products.png"
+    },
+    {
+        "id": 3,
+        "title": "Shopkeeper",
+        "path": "#/shopkeeper",
+        "imageLink": "/navbar/add-product.png"
+    },
+    {
+        "id": 4,
+        "title": "Items",
+        "path": "#/items",
+        "imageLink": "/navbar/bag.png"
+    },
+    {
+        "id": 5,
+        "title": "Co-ordinators",
+        "path": "#/co-ordinators",
+        "imageLink": "/navbar/write.png"
+    },
+    {
+        "id": 6,
+        "title": "Settings",
+        "path": "#/settings",
+        "imageLink": "/navbar/settings.png"
+    }
+];
